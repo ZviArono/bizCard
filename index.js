370f253f11108a34ee01589d834b52587b3c656c
ZviArono <zvi.aronwoitz@gmail.com>
2022-09-16 21:45:24 +0300

    deleted all flies on client side

--- index.js
+++ index.js
@@ -3,18 +3,8 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { engine } = require("express-handlebars");
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-// הוספת מנוע חדש לאפליקציה מסוג הנדלברס
-app.engine("handlebars", engine());
-// שיוך המנוע שהוספנו לאפליקציה
-app.set("view engine", "handlebars");
-// הגדרת הקבצים שאחראים על התצוגות המתחלפות
-app.set("views", "./views");
 
 // require routes
 const login = require("./routes/login");
@@ -29,35 +19,6 @@ app.use("/api/profile", profile);
 app.use("/api/cards", cards);
 app.use("/api/myCards", myCards);
 app.use(cors());
-
-// client side
-app.get("/", (req, res) => {
-  res.redirect("register");
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.get("/home", (req, res) => {
-  res.redirect("login");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-app.get("/cards", (req, res) => {
-  res.render("cards");
-});
-
-app.get("/myCards", (req, res) => {
-  res.render("myCards");
-});
-
-app.get("*", (req, res) => {
-  res.render("pnf");
-});
 
 mongoose
   .connect(process.env.dbString, { useNewUrlParser: true })
